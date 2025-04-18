488444c90875a46ec931fd448c0cfe3780c0a2ce
Pascal Noeltner <pascal@noeltner.net>
2025-04-18 12:47:07 +0200

    Chapter 8 Arrange TEST

---
+++ sample-next/refactored-app/__tests__/sum.test.ts
@@ -0,0 +1,17 @@
+import { sum } from "../helpers/sum";
+
+describe("the sum function", () => {
+    test("two plus two is four", () => {
+        let first = 2;
+        let second = 2;
+        let expectation = 4;
+    });
+
+    test("minus eight plus four", () => {
+        let first = -8;
+        let second = 4;
+        let expectation = -4;
+
+    });
+
+});
